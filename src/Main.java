65fb1b6c1b32e0172b6e599a1e5cc35d19eedbcf
diasway <mdias2006s@gmail.com>
2025-03-28 09:42:14 +0500

    2 Task linear

--- src/Main.java
+++ src/Main.java
@@ -13,35 +13,17 @@ public class Main {
             list.add(scanner.nextInt());
         }
         long startTime1 = System.nanoTime();
-        finder1(list, 0, Integer.MAX_VALUE, n);
+        finder(list, n);
         long endTime1 = System.nanoTime();
         double duration1 = (endTime1 - startTime1) / 1_000_000.0;
         System.out.println("finder1 execution time: " + duration1 + " ms");
-
-        long startTime2 = System.nanoTime();
-        finder2(list, n);
-        long endTime2 = System.nanoTime();
-        double duration2 = (endTime2 - startTime2) / 1_000_000.0;
-        System.out.println("finder2 execution time: " + duration2 + " ms");
     }
-    public static void finder1(ArrayList<Integer> list, int count, int min,int n){
-        if (count < n) {
-            if (min > list.get(count)) {
-                min = list.get(count);
-            }
-            finder1(list, count + 1,min,n);
-        } else {
-            System.out.println(min);
-        }
-    }
-    public static void finder2(ArrayList<Integer> list, int n){
-        int min = Integer.MAX_VALUE;
 
-        for (int i = 0; i < n; i++) {
-            if (min > list.get(i)) {
-                min = list.get(i);
-            }
+    public static void finder(ArrayList<Integer> list, int n){
+        int sum = 0;
+        for(int num : list){
+            sum += num;
         }
-        System.out.println(min);
+        System.out.println(1.0 * sum/n);
     }
 }
